4be32272449e2c459175e537713fa177b36739fa
Domenic <domenic.cassisi@web.de>
2017-10-18 18:28:02 +0200

    Added first test class

---
+++ src/de/fuwa/bomberman/tests/TestMain.java
@@ -0,0 +1,9 @@
+package de.fuwa.bomberman.tests;
+
+public class TestMain {
+
+    public static void main(String[] args) {
+        System.out.println("Herzlich willkommen zum Informatik Workshop!");
+    }
+
+}
