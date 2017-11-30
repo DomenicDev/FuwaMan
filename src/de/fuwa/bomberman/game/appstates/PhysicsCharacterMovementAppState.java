45ce991486cb7d92f6d2341eb7aba69f146c5646
DomenicDev <domenic.cassisi@web.de>
2017-11-30 15:19:18 +0100

    Finished the BombCollisionLogic in MovementAppState

--- src/de/fuwa/bomberman/game/appstates/PhysicsCharacterMovementAppState.java
+++ src/de/fuwa/bomberman/game/appstates/PhysicsCharacterMovementAppState.java
@@ -12,7 +12,6 @@ import de.fuwa.bomberman.game.components.PositionComponent;
 import de.fuwa.bomberman.game.components.WalkableComponent;
 import de.fuwa.bomberman.game.enums.MoveDirection;
 import de.fuwa.bomberman.game.utils.GameUtils;
-import javafx.geometry.Pos;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,6 +23,7 @@ public class PhysicsCharacterMovementAppState extends BaseAppState {
     private EntitySet bombEntities;
     private EntityData entityData;
 
+    // this map stores the character id with the bomb id it is in right now
     private Map<EntityId, EntityId> playerOnBomb = new HashMap<>();
 
     @Override
@@ -31,19 +31,23 @@ public class PhysicsCharacterMovementAppState extends BaseAppState {
         this.entityData = stateManager.getState(EntityDataState.class).getEntityData();
         this.characters = entityData.getEntities(PositionComponent.class, WalkableComponent.class, CollisionComponent.class);
         this.physicalObjects = entityData.getEntities(PositionComponent.class, CollisionComponent.class);
-        this.bombEntities = entityData.getEntities(PositionComponent.class, CollisionComponent.class, BombComponent.class)
+        this.bombEntities = entityData.getEntities(PositionComponent.class, CollisionComponent.class, BombComponent.class);
     }
 
     @Override
     public void update(float tpf) {
         physicalObjects.applyChanges();
         characters.applyChanges();
-        if(bombEntities.applyChanges()){
-            for(Entity bomb : bombEntities.getAddedEntities()){
+        // we have to check if there are some characters
+        // at the position of newly placed bombs
+        // if so we have to ignore collisions
+        // until the character walked out of the shape
+        if (bombEntities.applyChanges()) {
+            for (Entity bomb : bombEntities.getAddedEntities()) {
                 PositionComponent bombPos = bomb.get(PositionComponent.class);
-                for(Entity character : characters){
+                for (Entity character : characters) {
                     PositionComponent charPos = character.get(PositionComponent.class);
-                    if(GameUtils.inSameCell(charPos, bombPos){
+                    if (GameUtils.inSameCell(charPos, bombPos)) {
                         playerOnBomb.put(character.getId(), bomb.getId());
                     }
                 }
@@ -93,14 +97,15 @@ public class PhysicsCharacterMovementAppState extends BaseAppState {
                 // we prepare the rectangle of the other physical object
                 prepareRectangle(r2, physicalPos, physicalCollision);
 
+
                 // now we check if they intersect
                 // if they do we have a collision
-
-
                 if (areColliding(r1, r2)) {
-
-                    if(playerOnBomb.containsKey(character.getId())){
-                        if (playerOnBomb.get(character.getId()).equals(physicalObject.getId())){
+                    // if we collide we check if this character
+                    // is still "in" a bomb
+                    // if so, we have to ignore this collision
+                    if (playerOnBomb.containsKey(character.getId())) {
+                        if (playerOnBomb.get(character.getId()).equals(physicalObject.getId())) {
                             break;
                         }
                     }
@@ -108,6 +113,17 @@ public class PhysicsCharacterMovementAppState extends BaseAppState {
                     // means we do not apply the new calculated position
                     collided = true;
                     break;
+                } else {
+                    // in the case we did not collide
+                    // and the player is still contained in
+                    // the bomb list, we remove the character from the list
+                    if (playerOnBomb.containsKey(character.getId())) {
+                        if (playerOnBomb.get(character.getId()).equals(physicalObject.getId())) {
+                            playerOnBomb.remove(character.getId()); // remove it from the list
+                            collided = false; // just to make sure the value is set to false
+                            break; // we break out
+                        }
+                    }
                 }
 
             }
