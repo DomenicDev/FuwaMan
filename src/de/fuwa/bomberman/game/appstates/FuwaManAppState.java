cd5823284b522ee37d7bb1aaf3e1632001181c63
TheKey <schneider.niels@web.de>
2017-12-12 10:16:41 +0100

    Update FuwaManAppState.java

--- src/de/fuwa/bomberman/game/appstates/FuwaManAppState.java
+++ src/de/fuwa/bomberman/game/appstates/FuwaManAppState.java
@@ -60,7 +60,7 @@ public class FuwaManAppState extends BaseAppState implements GameMenuListener {
 
     @Override
     public void onClickWindow(){
-
+    context.getGraphicsConfiguration().getDevice().setFullScreenWindow(null);
     }
 
     @Override
