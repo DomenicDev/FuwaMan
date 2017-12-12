8a9e8684cd7107be146a311e808075e022c4cc16
DomenicDev <domenic.cassisi@web.de>
2017-12-12 09:14:49 +0100

    Added several player images

--- src/de/fuwa/bomberman/game/enums/ModelType.java
+++ src/de/fuwa/bomberman/game/enums/ModelType.java
@@ -2,24 +2,16 @@ package de.fuwa.bomberman.game.enums;
 
 public enum ModelType {
 
-    Player(null),
-    DestroyableTile("undestroyable.png"),
-    UndestroyableTile("undestroyable.png"),
-    Bomb("Bomb_Artwork.png"),
-    SpeedUp("SpeedUp.png"),
-    BombStrengthUp("BombStrengthUp.png"),
-    BombAmountUp("BombAmountUp.png"),
-    Explosion("Explosion.png");
+    Player,
+    Player2,
+    Player3,
+    Player4,
+    DestroyableTile,
+    UndestroyableTile,
+    Bomb,
+    SpeedUp,
+    BombStrengthUp,
+    BombAmountUp,
+    Explosion;
 
-
-    ModelType(String filename) {
-        this.filename = filename;
-    }
-
-    private String filename;
-
-
-    public String getFilename() {
-        return filename;
-    }
 }
