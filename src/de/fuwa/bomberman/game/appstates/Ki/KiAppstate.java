964b93d4776d6b21a5a0e9549891d09f8b59ff2c
DomenicDev <domenic.cassisi@web.de>
2017-12-12 09:16:02 +0100

    KI App State initial fix (Todo)

--- src/de/fuwa/bomberman/game/appstates/Ki/KiAppstate.java
+++ src/de/fuwa/bomberman/game/appstates/Ki/KiAppstate.java
@@ -91,33 +91,32 @@ public class KiAppstate extends BaseAppState {
                 else{
                     if(kiAction != null){
                         Path path = kiAction.getPath();
-                        for(int i = 0; i < path.getMoves().size()-1; i++){
-                            path.removeMove(path.getMove(i));
-                        }
-                        Move move = path.getMove(0);
-                        if (path.getMoves().size() > 0) {
-                            if (!playermap[(int) move.getPos().getX()][(int) move.getPos().getY()].isWalkable()) {
-                                MoveDirection movedir = move.getDir();
-                                PositionComponent newpos;
-                                if (movedir == MoveDirection.Up) {
-                                    movedir = MoveDirection.Down;
-                                    newpos = new PositionComponent(move.getPos().getX(), move.getPos().getY() + 1);
-                                } else if (movedir == MoveDirection.Down) {
-                                    movedir = MoveDirection.Up;
-                                    newpos = new PositionComponent(move.getPos().getX(), move.getPos().getY() - 1);
-                                } else if (movedir == MoveDirection.Left) {
-                                    movedir = MoveDirection.Right;
-                                    newpos = new PositionComponent(move.getPos().getX() + 1, move.getPos().getY());
-                                } else {
-                                    movedir = MoveDirection.Left;
-                                    newpos = new PositionComponent(move.getPos().getX() - 1, move.getPos().getY() + 1);
+                        if (path.getMoves().size() != 0) {
+                            for (int i = 0; i < path.getMoves().size() - 1; i++) {
+                                path.removeMove(path.getMove(i));
+                            }
+                            Move move = path.getMove(0);
+                            if (path.getMoves().size() > 0) {
+                                if (!playermap[(int) move.getPos().getX()][(int) move.getPos().getY()].isWalkable()) {
+                                    MoveDirection movedir = move.getDir();
+                                    PositionComponent newpos;
+                                    if (movedir == MoveDirection.Up) {
+                                        newpos = new PositionComponent(move.getPos().getX(), move.getPos().getY() + 1);
+                                    } else if (movedir == MoveDirection.Down) {
+                                        newpos = new PositionComponent(move.getPos().getX(), move.getPos().getY() - 1);
+                                    } else if (movedir == MoveDirection.Left) {
+                                        newpos = new PositionComponent(move.getPos().getX() + 1, move.getPos().getY());
+                                    } else {
+                                        newpos = new PositionComponent(move.getPos().getX() - 1, move.getPos().getY() + 1);
+                                    }
+                                    PositionComponent pos = entity.get(PositionComponent.class);
+                                    KiAction kiAction2 = new KiAction(AStar.findPath(pos, newpos, entityData, playermap, true, MapSizeX, MapSizeY), false, 0);
+                                    kiActions.put(entity.getId(), kiAction2);
+
+
+                                    System.out.println("test");
+                                    kidone.put(entity.getId(), false);
                                 }
-                                path.removeMove(move);
-                                path.addMove(new Move(newpos, movedir));
-
-
-                                System.out.println("test");
-                                kidone.put(entity.getId(), false);
                             }
                         }
                     }
