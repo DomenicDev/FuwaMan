0f5647a29b21031811c1d5a60a79cab9eb8b7628
DomenicDev <domenic.cassisi@web.de>
2017-12-14 14:32:32 +0100

    Implemented snow background

--- src/de/fuwa/bomberman/game/gui/BasicFuwaManPanel.java
+++ src/de/fuwa/bomberman/game/gui/BasicFuwaManPanel.java
@@ -11,13 +11,24 @@ public class BasicFuwaManPanel extends JPanel {
 
     private static final String PATH = "assets/Textures/";
     private static BufferedImage backgroundImage;
+    private static BufferedImage[] snowFrames;
+    private static int snowFrameCounter;
     private static BufferedImage titleImage;
     protected static GameMenuListener listener;
+    private static long lastTime = System.nanoTime();
 
     static {
         try {
-            backgroundImage = ImageIO.read(new File(PATH + "MENU_001.png"));
+            backgroundImage = ImageIO.read(new File(PATH + "Background_menu.gif"));
             titleImage = ImageIO.read(new File(PATH + "title.png"));
+
+            // load snow
+            BufferedImage snowSpriteSheet = ImageIO.read(new File(PATH + "snow_sprite.png"));
+            snowFrames = new BufferedImage[4];
+            snowFrames[0] = snowSpriteSheet.getSubimage(0, 0, 800, 800);
+            snowFrames[1] = snowSpriteSheet.getSubimage(800, 0, 800, 800);
+            snowFrames[2] = snowSpriteSheet.getSubimage(0, 800, 800, 800);
+            snowFrames[3] = snowSpriteSheet.getSubimage(800, 800, 800, 800);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -59,9 +70,21 @@ public class BasicFuwaManPanel extends JPanel {
     }
 
 
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+
+        long now = System.nanoTime();
+        if ((float) (now - lastTime) / 1000000000L > 0.4f) {
+            lastTime = now;
+            if (snowFrameCounter < 3) {
+                snowFrameCounter++;
+            } else {
+                snowFrameCounter = 0;
+            }
+        }
+        g.drawImage(snowFrames[snowFrameCounter], 0, 0, getWidth(), getHeight(), this);
     }
 }
