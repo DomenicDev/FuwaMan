1b39642293ba4a853637cd2a6df44ded82834c50
DomenicDev <domenic.cassisi@web.de>
2018-01-23 16:23:29 +0100

    Implemented full screen mode

--- src/de/fuwa/bomberman/app/gui/GameContextFrame.java
+++ src/de/fuwa/bomberman/app/gui/GameContextFrame.java
@@ -37,6 +37,8 @@ public class GameContextFrame extends JFrame {
         setLocationRelativeTo(null); // to place it in the middle of the screen
         setMinimumSize(new Dimension(1200, 720));
         setResizable(true);
+        setExtendedState(MAXIMIZED_BOTH);
+        setUndecorated(true);
         setPreferredSize(new Dimension(settings.getWidth(), settings.getHeight()));
         setLayout(new GridLayout(1,1));
 
